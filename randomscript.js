b349de4338108183bffe2a0834a07e370a7a5d1a
Kat <kat.dagenais@gmail.com>
2017-01-21 09:48:02 -0500

    On the way to checking answers

--- randomscript.js
+++ randomscript.js
@@ -12,11 +12,6 @@ window.onload = function(){
 	questionPicker(cat4Random, "cat4q1", "cat4q2", "cat4q3", "cat4q4", "cat4q5", "cat4q6");
 	questionPicker(cat5Random, "cat5q1", "cat5q2", "cat5q3", "cat5q4", "cat5q5", "cat5q6");
 	questionPicker(cat6Random, "cat6q1", "cat6q2", "cat6q3", "cat6q4", "cat6q5", "cat6q6");
-
-//please paste here
-
-}
-
 $(document).ready(function() 
 {    $("#results").click(function() {                
 
@@ -25,11 +20,7 @@ if (!$("input[@name=q1]:checked").val() ||
 !$("input[@name=q3]:checked").val() ||            
 !$("input[@name=q4]:checked").val() ||            
 !$("input[@name=q5]:checked").val() ||            
-!$("input[@name=q6]:checked").val() ||            
-!$("input[@name=q7]:checked").val() ||            
-!$("input[@name=q8]:checked").val() ||            
-!$("input[@name=q9]:checked").val() ||            
-!$("input[@name=q10]:checked").val()            
+!$("input[@name=q6]:checked").val()            
 ) {            
 alert("You're not done yet!");        
 }        
@@ -40,11 +31,7 @@ var cat2name = "2";
 var cat3name = "3";            
 var cat4name = "4";            
 var cat5name = "5";            
-var cat6name = "6";            
-var cat7name = "7";            
-var cat8name = "8";            
-var cat9name = "9";            
-var cat10name = "10";            
+var cat6name = "6";                      
 var cat11name = "None";            
             
 
@@ -60,15 +47,7 @@ var cat5 = ($("input[@name=q5]:checked").val() != "a");
 
 var cat6 = ($("input[@name=q6]:checked").val() != "b");  
 
-var cat7 = ($("input[@name=q7]:checked").val() != "c"); 
-
-var cat8 = ($("input[@name=q8]:checked").val() != "d");  
-
-var cat9 = ($("input[@name=q9]:checked").val() != "a"); 
-
-var cat10 = ($("input[@name=q10]:checked").val() != "b");  
-
-var cat11 = (!cat1 && !cat2 && !cat3 && !cat4 && !cat5 && !cat6 && !cat7 && !cat8 && !cat9 && !cat10); var categories = [];                        
+var cat11 = (!cat1 && !cat2 && !cat3 && !cat4 && !cat5 && !cat6); var categories = [];                        
 
 if (cat1) { categories.push(cat1name) };            
 if (cat2) { categories.push(cat2name) };            
@@ -76,10 +55,6 @@ if (cat3) { categories.push(cat3name) };
 if (cat4) { categories.push(cat4name) };            
 if (cat5) { categories.push(cat5name) };            
 if (cat6) { categories.push(cat6name) };            
-if (cat7) { categories.push(cat7name) };            
-if (cat8) { categories.push(cat8name) };            
-if (cat9) { categories.push(cat9name) };            
-if (cat10) { categories.push(cat10name) };            
 if (cat11) { categories.push(cat11name) };                        
 
 var catStr = 'You answered the following questions incorrectly: ' + categories.join(', ') + '';                     
@@ -91,13 +66,12 @@ if (cat2) { $("#category2").show("slow"); };
 if (cat3) { $("#category3").show("slow"); };            
 if (cat4) { $("#category4").show("slow"); };            
 if (cat5) { $("#category5").show("slow"); };            
-if (cat6) { $("#category6").show("slow"); };            
-if (cat7) { $("#category7").show("slow"); };            
-if (cat8) { $("#category8").show("slow"); };            
-if (cat9) { $("#category9").show("slow"); };            
-if (cat10) { $("#category10").show("slow"); };            
+if (cat6) { $("#category6").show("slow"); };             
 if (cat11) { $("#category11").show("slow"); };
 { $("#closing").show("slow"); };
+}
+
+
 }
 
 function questionPicker(var rand, var q1, var q2, var q3, var q4, var q5, var q6)
